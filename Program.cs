77bf1149b7e06e2694173de926c3f172c972aa3c
LAPTOP-CDM690QG\ACER <naruepon.ice@mail.kmutt.ac.th>
2021-03-28 02:25:06 +0700

    Part2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                     
+            Console.Write("Input a : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Input b : ");
+            int b = int.Parse(Console.ReadLine());
+            int x = a, y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }                
+            }
+            Console.WriteLine("X = " + x);            
+        }
+    }
+}
